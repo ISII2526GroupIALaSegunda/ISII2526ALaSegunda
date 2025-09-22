bd5109a5f3e964e049df3909dbb9abdfcf05a7f4
HÉCTOR RUÍZ LÓPEZ <Hector.Ruiz2@alu.uclm.es>
2025-09-22 17:20:22 +0200

    Adding PurchaseProduct with the attributes

---
+++ src/AppForSEII2526.API/Models/PurchaseProduct.cs
@@ -0,0 +1,20 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class PurchaseProduct
+    {
+        [Key]
+        public int Id { get; set; }  
+
+        public int PurchaseOrderId { get; set; }
+   
+        public int ProductId { get; set; }
+
+        [Precision(10, 2)]
+        public decimal Price { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "You must provide a quantity higher than 1")]
+        public int Quantity { get; set; }
+
+    
+
+    }
+}
