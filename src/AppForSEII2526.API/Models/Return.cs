f711c6cc1ea3dbde731faffab26b6f7161c62164
JUAN MIGUEL SANZ ENE <JuanMiguel.Sanz@alu.uclm.es>
2025-09-22 17:19:56 +0200

    adding several attributes to return

---
+++ src/AppForSEII2526.API/Models/Return.cs
@@ -0,0 +1,25 @@
+﻿namespace AppForSEII2526.API.Models
+{        
+    
+    [Index(nameof(Name), IsUnique = true)]
+    public class Return
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public double MoneyToReturn { get; set; }
+
+       [Precision(5,2)]
+        public decimal NewTotalPrice { get; set; }
+
+        public int? Rating { get; set; }
+
+        [Precision(5, 2)]
+        public decimal TotalPrice { get; set; }
+    }
+
+   
+}
