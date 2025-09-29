107fafc356458e73705d60297f3757dd71afd94a
JAIME SÁNCHEZ PARADA <Jaime.Sanchez19@alu.uclm.es>
2025-09-29 18:00:10 +0200

    Add BanReport Class

---
+++ src/AppForSEII2526.API/Models/BanReport.cs
@@ -0,0 +1,21 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class BanReport
+    {
+        public int ID { get; set; }
+
+
+        [StringLength(100, ErrorMessage = "Description should not be longer than 100 characters")]
+        public string DetailedDescription { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime EndDate { get; set; }
+
+        [StringLength(100, ErrorMessage = "Reason should not be longer than 100 characters")]
+        public string Reason {  get; set; }
+
+        
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime StartDate { get; set; }
+    }
+}
