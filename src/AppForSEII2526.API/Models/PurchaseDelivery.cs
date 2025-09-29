11d02a5487b00499a653e70c9f984cb1d251b745
alejandrogomezl <alejandrogomezlozano@hotmail.com>
2025-09-29 16:52:47 +0200

    [IMP] Implement Purchase Delivery Class with relation to DeliveryAssignment

---
+++ src/AppForSEII2526.API/Models/PurchaseDelivery.cs
@@ -0,0 +1,9 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class PurchaseDelivery
+    {
+        public DateTime Date { get; set; }
+
+        public DeliveryAssignment DeliveryAssignment { get; set; }
+    }
+}
