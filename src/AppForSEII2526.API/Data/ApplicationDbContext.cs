3869d735fbda7c94dce209adca5f17737b1cce42
Héctor <82473637+Ecomont@users.noreply.github.com>
2025-09-29 17:36:30 +0200

    PR:Add PurchaseOrder entity and DbContext integration (#21)
    Close #21
    
    Add PurchaseOrder entity and DbContext integration

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -6,5 +6,6 @@ namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
     public DbSet<DeliveryAssignment> DeliveryAssignments { get; set; }
+    public DbSet<PurchaseOrder> PurchaseOrders { get; set; }
 
 }
