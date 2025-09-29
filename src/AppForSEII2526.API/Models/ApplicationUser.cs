f00b8533b7aecd1b47c54f54ec86cd18755fbb4b
JUAN MIGUEL SANZ ENE <JuanMiguel.Sanz@alu.uclm.es>
2025-09-29 14:15:12 +0200

    added all classes for Return Products of a Purchase

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -3,6 +3,16 @@
 namespace AppForSEII2526.API.Models;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
+
+[Index(nameof(Name), IsUnique = true)]
+
 public class ApplicationUser : IdentityUser {
 
+    public DateTime AccountCreationDate { get; set; }
+
+    public string Address { get; set; }
+
+    public string Name { get; set; }
+
+    public string Surname { get; set; }
 }
---
+++ src/AppForSEII2526.API/Models/ReturnProduct.cs
@@ -0,0 +1,11 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class ReturnProduct
+    {
+        public int Id { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string Reason { get; set; }
+    }
+}
