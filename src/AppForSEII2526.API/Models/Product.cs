2da8b35414a39d23e3ce062c401f3463f37448c8
Ecomont <hector19.rl@gmail.com>
2025-09-30 22:28:39 +0200

    Add Product entity and integrate with ApplicationDbContext

---
+++ src/AppForSEII2526.API/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(80)]
+        public string Title { get; set; } = default!;
+
+        [Precision(10, 2)]
+        [Range(typeof(decimal), "0.10", "1000000", ErrorMessage = "Price must be at least 0.10")]
+        public decimal Price { get; set; }
+
+        public int Stock { get; set; }
+
+
+    }
+}
