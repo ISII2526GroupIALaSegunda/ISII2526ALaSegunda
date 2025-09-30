406d620c66d8341d3db668679d671587e8c92673
Alejandro Gómez Lozano <49101889+alejandrogomezl@users.noreply.github.com>
2025-09-30 23:48:43 +0200

    Task8 create purchase delivery class (#31)
    Closed task 8
    
    * [IMP] Implement Purchase Delivery Class with relation to DeliveryAssignment
    
    * [IMP] Priority in Purchase Delivery and add to application db context
    
    * [FIX] Add Primary Key to Purchase Delivery
    
    * [IMP] Add DeliveryAsignmentId to PurchaseDelivery class as Primary Key

---
+++ src/AppForSEII2526.API/Models/PurchaseDelivery.cs
@@ -0,0 +1,22 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    [PrimaryKey(nameof(DeliveryAssignmentId))]
+    public class PurchaseDelivery
+    {
+        public int DeliveryAssignmentId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public DeliveryAssignment DeliveryAssignment { get; set; }
+
+        public PriorityType Priority { get; set; }
+
+    }
+
+    public enum PriorityType
+    {
+        Low,
+        Medium,
+        High
+    }
+}
