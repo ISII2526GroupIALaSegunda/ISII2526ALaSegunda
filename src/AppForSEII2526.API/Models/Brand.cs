498b1eeb85196caf01d4674ec31aa3e2983dbef8
Ecomont <hector19.rl@gmail.com>
2025-10-01 22:47:31 +0200

    Fix

--- src/AppForSEII2526.API/Models/Brand.cs
+++ src/AppForSEII2526.API/Models/Brand.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
 
         [Required, StringLength(80)]
-        public string Name { get; set; };
+        public string Name { get; set; }
 
         public string? Location { get; set; }
     }
