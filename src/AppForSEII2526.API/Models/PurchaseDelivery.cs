41e77bc364be6dddbfe5c9a2f82895029018d6ee
Alejandro Gómez Lozano <49101889+alejandrogomezl@users.noreply.github.com>
2025-10-01 20:30:45 +0200

    Task38 relation purchase order with purchase delivery (#40)
    Close task 38
    
    * [IMP] Relation Purchase Delivery and Purchase order
    
    * [IMP] Update Class Diagram

--- src/AppForSEII2526.API/Models/PurchaseDelivery.cs
+++ src/AppForSEII2526.API/Models/PurchaseDelivery.cs
@@ -1,15 +1,18 @@
 ﻿namespace AppForSEII2526.API.Models
 {
-    [PrimaryKey(nameof(DeliveryAssignmentId))]
+    [PrimaryKey(nameof(DeliveryAssignmentId),nameof(PurchaseOrderId))]
     public class PurchaseDelivery
     {
         public int DeliveryAssignmentId { get; set; }
+        public int PurchaseOrderId { get; set; }
 
         public DateTime Date { get; set; }
 
         public DeliveryAssignment DeliveryAssignment { get; set; }
 
         public PriorityType Priority { get; set; }
+
+        public PurchaseOrder PurchaseOrder { get; set; }
 
     }
 
