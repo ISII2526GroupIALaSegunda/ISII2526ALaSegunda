a21647efaeec036af6bca96b365e9ce1649e6eb6
JaimeSanchez19 <Jaime.Sanchez19@alu.uclm.es>
2025-10-02 17:30:43 +0200

    PR:Add reportstate (#49)
    Close task 23
    
    * Add reportstate
    
    * Changes
    
    * Elimination of the reportState Class due to it not being correctly made
    
    * Changes in the enum and the applicationBdContext

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -22,5 +22,5 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     public DbSet<Complaint> Complaints { get; set; }
 
     public DbSet<ApplicationUser> ApplicationUsers{ get; set;}
-    public DbSet<ComplaintType> ComplaintTypes { get; set; }
+    public DbSet<ReportCustomer> ReportCustomers { get; set; }
 }
--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
 
 namespace AppForSEII2526.API.Models;
 
@@ -18,6 +19,9 @@ public class ApplicationUser : IdentityUser {
 
     public DateTime AccountCreationDate { get; set; }
     public IList<PurchaseOrder>? PurchaseOrders { get; set; }
+    
+
+
 
 
 
