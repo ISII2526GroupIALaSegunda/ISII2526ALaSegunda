0192b612b032e0e65a380a621f5a24cf06c78a6a
JAIME SÁNCHEZ PARADA <Jaime.Sanchez19@alu.uclm.es>
2025-10-02 11:23:36 +0200

    Added Complaint Class

---
+++ src/AppForSEII2526.API/Models/Complaint.cs
@@ -0,0 +1,15 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Complaint
+    {
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime ComplaintDate { get; set; }
+
+        [StringLength(100, ErrorMessage = "Description should not be longer than 100 characters")]
+        public string Description { get; set; }
+
+        public int ID { get; set; }
+
+        public bool Processed { get; set; }
+    }
+}
