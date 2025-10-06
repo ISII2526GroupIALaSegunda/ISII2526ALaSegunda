0d2e21bbd01a96ef5c415e311ce5df924cf750b2
alejandrogomezl <alejandrogomezlozano@hotmail.com>
2025-10-06 17:18:20 +0200

    [FIX] Change Assignments rlation to DeliveryAssignment

--- src/AppForSEII2526.API/Models/DeliveryDriver.cs
+++ src/AppForSEII2526.API/Models/DeliveryDriver.cs
@@ -2,7 +2,7 @@
 {
     public class DeliveryDriver
     {
-        public List<PurchaseDelivery> Assignments { get; set; }
+        public IList<DeliveryAssignment> Assignments { get; set; }
         public bool Available { get; set; }
         public int id { get; set; }
         [StringLength(20, ErrorMessage = "Name can not be longer than 200 characters")]
