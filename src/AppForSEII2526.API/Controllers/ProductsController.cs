042ed22e2ae864e899dae6f2ef6695109605d724
HÉCTOR RUÍZ LÓPEZ <Hector.Ruiz2@alu.uclm.es>
2025-10-13 17:14:55 +0200

    Add ProductsController

---
+++ src/AppForSEII2526.API/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        private ILogger<ProductsController> _logger;
+
+        public ProductsController(ApplicationDbContext context, ILogger<ProductsController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        //[HttpGet]
+        //[Route("[action]")]
+        //[ProducesResponseType(typeof(decimal), (int)HttpStatusCode.OK)]
+        //public async Task<ActionResult> ComputeDivision(decimal op1, decimal op2)
+        //{
+        //    if (op2 == 0)
+        //    {
+        //        _logger.LogError($"{DateTime.Now} Exception: op2=0, division by 0");
+        //        return BadRequest("op2 must be different from 0");
+        //    }
+        //    decimal result = op1 / op2;
+        //    return Ok(result);
+        //}
+    }
+}
