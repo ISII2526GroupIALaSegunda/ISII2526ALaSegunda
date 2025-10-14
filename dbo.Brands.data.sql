9ccd43512b9f54e8e0cb8efedab500938f9df243
Ecomont <hector19.rl@gmail.com>
2025-10-14 04:19:25 +0200

    Add data insertion for Brands and Products tables

---
+++ dbo.Brands.data.sql
@@ -0,0 +1,4 @@
+﻿SET IDENTITY_INSERT [dbo].[Brands] ON
+INSERT INTO [dbo].[Brands] ([Id], [Location], [Name]) VALUES (1, N'Madrid', N'Zara')
+INSERT INTO [dbo].[Brands] ([Id], [Location], [Name]) VALUES (2, N'Barcelona', N'Uniqlo')
+SET IDENTITY_INSERT [dbo].[Brands] OFF
---
+++ src/AppForSEII2526.API/Data/dbo.Products.data.sql
@@ -0,0 +1,10 @@
+﻿SET IDENTITY_INSERT [dbo].[Brands] ON
+INSERT INTO [dbo].[Brands] ([Id], [Location], [Name]) VALUES (1, N'Madrid', N'Zara')
+INSERT INTO [dbo].[Brands] ([Id], [Location], [Name]) VALUES (2, N'Barcelona', N'Uniqlo')
+SET IDENTITY_INSERT [dbo].[Brands] OFF
+
+
+SET IDENTITY_INSERT [dbo].[Products] ON
+INSERT INTO [dbo].[Products] ([ProductId], [Name], [Description], [Colour], [Price], [Stock], [IsReturnable], [BrandId]) VALUES (1, N'Jacket', NULL, NULL, CAST(20.00 AS Decimal(10, 2)), 4, 1, 1)
+INSERT INTO [dbo].[Products] ([ProductId], [Name], [Description], [Colour], [Price], [Stock], [IsReturnable], [BrandId]) VALUES (2, N'Shirt', NULL, NULL, CAST(10.00 AS Decimal(10, 2)), 2, 0, 2)
+SET IDENTITY_INSERT [dbo].[Products] OFF
