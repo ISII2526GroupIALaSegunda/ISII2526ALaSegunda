42a6e0bcd0c90316489215e2167a68b8ed6bb088
Ecomont <hector19.rl@gmail.com>
2025-10-21 07:24:28 +0200

    Add PurchasesController and related DTOs for purchases
    Introduced a new `PurchasesController` to handle purchase-related API endpoints, including a `GetPurchase` method for retrieving detailed purchase information with multiple database joins.
    
    Added `PurchaseForCreateDTO`, `PurchaseForDetailDTO`, and `PurchaseItemDTO` to define data structures for creating and retrieving purchase data. These DTOs include validation attributes, object comparison methods, and support for mapping complex relationships.
    
    Implemented logging and error handling in the controller to ensure robustness.

---
+++ src/AppForSEII2526.API/DTOs/PurchaseDTOs/PurchaseItemDTO.cs
@@ -0,0 +1,43 @@
+﻿namespace AppForSEII2526.API.DTOs.PurchaseDTOs
+{
+    public class PurchaseItemDTO
+    {
+        public PurchaseItemDTO(int productId, string name, string brand, string? colour, decimal unitPrice, int quantity)
+        {
+            ProductId = productId;
+            Name = name;
+            Brand = brand;
+            Colour = colour;
+            UnitPrice = unitPrice;
+            Quantity = quantity;
+        }
+
+        public int ProductId { get; set; }
+
+        [Required, StringLength(100, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required, StringLength(100)]
+        public string Brand { get; set; }
+
+        [StringLength(30)]
+        public string? Colour { get; set; }
+
+        [Precision(10, 2)]
+        public decimal UnitPrice { get; set; }   // snapshot de Products.Price
+
+        [Range(1, int.MaxValue)]
+        public int Quantity { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is PurchaseItemDTO dTO &&
+                   ProductId == dTO.ProductId &&
+                   Name == dTO.Name &&
+                   Brand == dTO.Brand &&
+                   Colour == dTO.Colour &&
+                   UnitPrice == dTO.UnitPrice &&
+                   Quantity == dTO.Quantity;
+        }
+    }
+}
