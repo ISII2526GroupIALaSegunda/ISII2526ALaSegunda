343edfce4d8f63b01f7e2448241bb7a8e93ef072
Ecomont <hector19.rl@gmail.com>
2025-10-22 08:03:16 +0200

    Remove specific brand insertions from SQL script

--- dbo.Brands.data.sql
+++
@@ -1,4 +0,0 @@
-﻿SET IDENTITY_INSERT [dbo].[Brands] ON
-INSERT INTO [dbo].[Brands] ([Id], [Location], [Name]) VALUES (1, N'Madrid', N'Zara')
-INSERT INTO [dbo].[Brands] ([Id], [Location], [Name]) VALUES (2, N'Barcelona', N'Uniqlo')
-SET IDENTITY_INSERT [dbo].[Brands] OFF
