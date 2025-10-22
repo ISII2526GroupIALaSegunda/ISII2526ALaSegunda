ac5192ce84e762556f861097b1d21dba7354a162
JUAN MIGUEL SANZ ENE <JuanMiguel.Sanz@alu.uclm.es>
2025-10-22 23:52:26 +0200

    added SelectPurchaseProductsForReturning

--- src/AppForSEII2526.API/Controllers/PurchaseProductsController.cs
+++ src/AppForSEII2526.API/Controllers/PurchaseProductsController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AppForSEII2526.API.DTOs_PurchaseProductDTOs;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace AppForSEII2526.API.Controllers
 {
@@ -31,5 +33,38 @@ namespace AppForSEII2526.API.Controllers
         //    decimal result = op1 / op2;
         //    return Ok(result);
         //}
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<PurchaseProductsForReturningDTO>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ModelError), (int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult> GetPurchaseProductsForReturning(string? productName, int quantity, string userName
+            ){ 
+            
+            //if(!isReturnable)
+            //{
+            //    ModelState.AddModelError("isReturnable", "Product has already been returned");
+            //    _logger.LogError($"Product must have not been returned yet");
+            //    return BadRequest(new ValidationProblemDetails(ModelState));
+            //}
+
+            IList<PurchaseProductsForReturningDTO> purchaseProductsDTOS = await _context.PurchaseProducts
+                
+                .Include(purchaseProduct => purchaseProduct.Product)
+                .Include(purchaseProduct => purchaseProduct.ReturnProduct)
+                .Include(purchaseProduct => purchaseProduct.Product.Brand)
+
+                .Where(purchaseProduct=>(purchaseProduct.Product.IsReturnable)
+                
+                &&(purchaseProduct.PurchaseOrder.Customer.UserName == userName)
+
+                &&(purchaseProduct.ReturnProduct != null))
+
+                .Select(purchaseProduct=>new PurchaseProductsForReturningDTO(purchaseProduct.ProductId, purchaseProduct.Product.Name,
+                purchaseProduct.ReturnProduct.Quantity, purchaseProduct.Product.Brand.Name, purchaseProduct.Product.Brand.Location, purchaseProduct.Product.IsReturnable))
+                .ToListAsync();
+            return Ok(purchaseProductsDTOS);
+        }
+    
     }
 }
--- src/AppForSEII2526.API/Data/dbo.Products.data.sql
+++ src/AppForSEII2526.API/Data/dbo.Products.data.sql
@@ -1,4 +1,4 @@
 ﻿SET IDENTITY_INSERT [dbo].[Products] ON
-INSERT INTO [dbo].[Products] ([ProductId], [Name], [Description], [Colour], [Price], [Stock], [IsReturnable], [BrandId]) VALUES (2, N'Product2', N'Second Product', N'Red', CAST(50.00 AS Decimal(10, 2)), 20, 1, 2)
-INSERT INTO [dbo].[Products] ([ProductId], [Name], [Description], [Colour], [Price], [Stock], [IsReturnable], [BrandId]) VALUES (3, N'Product3', N'Third Product', N'Blue', CAST(75.00 AS Decimal(10, 2)), 15, 0, 1)
+INSERT INTO [dbo].[Products] ([ProductId], [Name], [Description], [Colour], [Price], [Stock], [IsReturnable], [BrandId]) VALUES (2, N'ProductCok', N'Second Product', N'Red', CAST(50.00 AS Decimal(10, 2)), 20, 1, 2)
+INSERT INTO [dbo].[Products] ([ProductId], [Name], [Description], [Colour], [Price], [Stock], [IsReturnable], [BrandId]) VALUES (3, N'Product3', N'Third Product', N'Blue', CAST(75.00 AS Decimal(10, 2)), 15, 1, 1)
 SET IDENTITY_INSERT [dbo].[Products] OFF
