129b3e51329646a74be0ecb9dfe2e0e646541538
alejandrogomezl <alejandrogomezlozano@hotmail.com>
2025-10-26 19:03:37 +0100

    [IMP] Delivery Assignment and purchase Delivery DTOs created

---
+++ src/AppForSEII2526.API/DTOs/DeliveryAssignmentDTOs/DeliveryAssignmentForDetailDTO.cs
@@ -0,0 +1,27 @@
+﻿namespace AppForSEII2526.API.DTOs.DeliveryAssignmentDTOs
+{
+    public class DeliveryAssignmentForDetailDTO
+    {
+        public DeliveryAssignmentForDetailDTO(int id, string deliveryDriverName, DateTime deliveryAssignmentDone, string personalMessage, decimal extraReward, IList<PurchaseDeliveryForDetailDTO> purchaseDeliveries)
+        {
+            Id = id;
+            DeliveryDriverName = deliveryDriverName;
+            DeliveryAssignmentDone = deliveryAssignmentDone;
+            PersonalMessage = personalMessage;
+            ExtraReward = extraReward;
+            PurchaseDeliveries = purchaseDeliveries;
+        }
+        public int Id { get; set; }
+        [StringLength(20, ErrorMessage = "Name can not be longer than 200 characters")]
+        public string DeliveryDriverName { get; set; }
+        public DateTime DeliveryAssignmentDone { get; set; }
+        [Precision(5, 2)]
+        public decimal ExtraReward { get; set; }
+
+        [StringLength(20, ErrorMessage = "Personal Message can be neither longer than 200 characters")]
+        public string PersonalMessage { get; set; }
+
+        public IList<PurchaseDeliveryForDetailDTO> PurchaseDeliveries { get; set; }
+
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/DeliveryAssignmentDTOs/PurchaseDeliveryForDetailDTO.cs
@@ -0,0 +1,31 @@
+﻿namespace AppForSEII2526.API.DTOs.DeliveryAssignmentDTOs
+{
+    public class PurchaseDeliveryForDetailDTO
+    {
+        public PurchaseDeliveryForDetailDTO(int id, DateTime date, string street, string city, string postalCode, decimal totalPrice, PriorityType priority )
+        {
+            Id = id;
+            Date = date;
+            Street = street;
+            City = city;
+            PostalCode = postalCode;
+            TotalPrice = totalPrice;
+            Priority = priority;
+        }
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public PriorityType Priority { get; set; }
+        [Required, StringLength(60)]
+        public string City { get; set; }
+
+        [Required, StringLength(120)]
+        public string Street { get; set; }
+
+        [Required, StringLength(20)]
+        public string PostalCode { get; set; }
+        [Precision(10, 2)]
+        [Range(typeof(decimal), "0.00", "9999999999.99")]
+        public decimal TotalPrice { get; set; }
+
+    }
+}
