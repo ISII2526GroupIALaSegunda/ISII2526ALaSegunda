1d312c03e4d06854558330dcd87a72566117d7bd
JUAN MIGUEL SANZ ENE <JuanMiguel.Sanz@alu.uclm.es>
2025-10-27 16:48:33 +0100

    created unique .sql with all the data

--- src/AppForSEII2526.API/Data/dbo.AspNetUsers.data.sql
+++
@@ -1,6 +0,0 @@
-﻿INSERT INTO [dbo].[AspNetUsers] 
-([Id], [Name], [Surname], [Address], [AccountCreationDate], [UserName], [NormalizedUserName], [Email], [NormalizedEmail], [EmailConfirmed], [PasswordHash], [SecurityStamp], [ConcurrencyStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEnd], [LockoutEnabled], [AccessFailedCount]) 
-VALUES 
-(N'1', N'User1', N'Userson', N'Elm Street', '2003-12-25 00:00:00', N'TheUser1', N'User1', N'User1@gmail.com', N'User1gmail', 1, N'Password1', N'Security1', N'Concurrency1', N'111111111', 1, 1, '2025-10-14 00:00:00', 1, 2),
-
-(N'2', N'User2', N'Usersen', N'Eln Street', '2003-12-26 00:00:00', N'TheUser2', N'User2', N'User2@gmail.com', N'User2gmail', 0, N'Password2', N'Security2', N'Concurrency2', N'222222222', 0, 0, '2025-10-15 00:00:00', 0, 3);
--- src/AppForSEII2526.API/Data/dbo.PaymentMethods.data.sql
+++
@@ -1,3 +0,0 @@
-﻿SET IDENTITY_INSERT [dbo].[PaymentMethods] ON
-INSERT INTO [dbo].[PaymentMethods] ([Id], [UserId], [Discriminator], [TelephoneNumber], [CreditCardNumber], [ExpirationDate], [Email]) VALUES (2, N'1', N'1', N'111111111', N'000000000', N'2026-10-31 00:00:00', N'User1@gmail.com')
-SET IDENTITY_INSERT [dbo].[PaymentMethods] OFF
