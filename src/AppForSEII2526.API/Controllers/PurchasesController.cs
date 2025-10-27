8320181f97db5221ccc3a22217bd72ba02f73e94
Alejandro Gómez Lozano <49101889+alejandrogomezl@users.noreply.github.com>
2025-10-27 17:06:47 +0100

    [FIX] Refactor purchase namespace (#102)

--- src/AppForSEII2526.API/Controllers/PurchasesController.cs
+++ src/AppForSEII2526.API/Controllers/PurchasesController.cs
@@ -1,5 +1,4 @@
-﻿using AppForSEII2526.API.DTOs.PurchaseDTOs;
-using AppForSEII2526.API.DTOs.PurchaseOrderDTOs;
+﻿using AppForSEII2526.API.DTOs.PurchaseOrderDTOs;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
