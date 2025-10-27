22d12e5dc98b886836047716cf05115c23cdd8f5
alejandrogomezl <alejandrogomezlozano@hotmail.com>
2025-10-27 17:28:46 +0100

    [IMP] Purchase Order for assignment DTO

---
+++ src/AppForSEII2526.API/DTOs/DeliveryAssignmentDTOs/PurchaseOrderForAssignmentDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace AppForSEII2526.API.DTOs.DeliveryAssignmentDTOs
+{
+    public class PurchaseOrderForAssignmentDTO{
+        public PurchaseOrderForAssignmentDTO(int purchaseOrderId, PriorityType priority)
+        {
+            PurchaseOrderId = purchaseOrderId;
+            Priority = priority;
+        }
+
+        [Required(ErrorMessage = "Purchase order ID is required")]
+        public int PurchaseOrderId { get; set; }
+
+        [Required(ErrorMessage = "Priority is required")]
+        public PriorityType Priority { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is PurchaseOrderForAssignmentDTO dTO &&
+                   PurchaseOrderId == dTO.PurchaseOrderId &&
+                   Priority == dTO.Priority;
+        }
+    }
+}
