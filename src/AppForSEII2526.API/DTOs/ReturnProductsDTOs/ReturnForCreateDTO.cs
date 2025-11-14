044ec29aad475529f44d74f412db684dd52b1941
JUAN MIGUEL SANZ ENE <JuanMiguel.Sanz@alu.uclm.es>
2025-11-14 07:15:32 +0100

    created DTOs for the POST method

---
+++ src/AppForSEII2526.API/DTOs/ReturnProductsDTOs/ReturnForCreateDTO.cs
@@ -0,0 +1,30 @@
+﻿namespace AppForSEII2526.API.DTOs.ReturnProductsDTOs
+{
+    public class ReturnForCreateDTO
+    {
+        public ReturnForCreateDTO(
+            int purchaseOrderId,
+            string userNameCustomer,
+            string reason,
+            int paymentMethod,
+            int? rating,
+            List<ReturnItemForCreateDTO> returnItems)
+        {
+            PurchaseOrderId = purchaseOrderId;
+            UserNameCustomer = userNameCustomer;
+            Reason = reason;
+            PaymentMethod = paymentMethod;
+            Rating = rating;
+            ReturnItems = returnItems;
+        }
+
+        public int PurchaseOrderId { get; set; }
+        public string UserNameCustomer { get; set; }  
+
+        public string Reason { get; set; }            
+        public int PaymentMethod { get; set; }     
+        public int? Rating { get; set; }              
+
+        public List<ReturnItemForCreateDTO> ReturnItems { get; set; }
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/ReturnProductsDTOs/ReturnItemForCreateDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace AppForSEII2526.API.DTOs.ReturnProductsDTOs
+{
+    public class ReturnItemForCreateDTO
+    {
+
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
