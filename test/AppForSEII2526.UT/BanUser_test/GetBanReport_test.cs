a9a2d6f18341ee31b8bdab14e09e49e9bdc2f324
JAIME SÁNCHEZ PARADA <Jaime.Sanchez19@alu.uclm.es>
2025-11-14 21:49:55 +0100

    fixing grammar

--- test/AppForSEII2526.UT/BanUser_test/GetBanReport_test.cs
+++ test/AppForSEII2526.UT/BanUser_test/GetBanReport_test.cs
@@ -109,7 +109,7 @@ namespace AppForSEII2526.UT.BanReportsController_test
                 "User repeatedly violated rules",
                 new DateTime(2025, 11, 14),
                 new DateTime(2025, 12, 14),
-                "In proggress",
+                "In progress",
                 new List<ReportCustomerForDetailDTO>
                 {
                     new ReportCustomerForDetailDTO(1, "Juan", "García", "Your account has been banned.")
