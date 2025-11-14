455052dd3acc6b8d263ae2e8c0fcade0cad25cb8
JUAN MIGUEL SANZ ENE <JuanMiguel.Sanz@alu.uclm.es>
2025-11-14 03:07:34 +0100

    created necessary DTO's for second get

---
+++ src/AppForSEII2526.API/DTOs/ReturnProductsDTOs/ReturnSummaryDTO.cs
@@ -0,0 +1,41 @@
+﻿namespace AppForSEII2526.API.DTOs.ReturnProductsDTOs
+{
+    public class ReturnSummaryDTO
+    {
+        public ReturnSummaryDTO(
+            
+            
+            
+ 
+            string customerName,
+            string customerSurname,
+            string customerAddress,
+            string customerPhoneNumber,
+            List<ReturnedProductInfoDTO> returnedProducts
+        )
+        {
+       
+
+            CustomerName = customerName;
+            CustomerSurname = customerSurname;
+            CustomerAddress = customerAddress;
+            CustomerPhoneNumber = customerPhoneNumber;
+
+            ReturnedProducts = returnedProducts;
+        }
+
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int PaymentMethod { get; set; }
+        public string Reason { get; set; }
+        public int? Rating { get; set; }
+        public double MoneyToReturn { get; set; }
+
+        public string CustomerName { get; set; }
+        public string CustomerSurname { get; set; }
+        public string CustomerAddress { get; set; }
+        public string CustomerPhoneNumber { get; set; }
+
+        public List<ReturnedProductInfoDTO> ReturnedProducts { get; set; }
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/ReturnProductsDTOs/ReturnedProductInfoDTO.cs
@@ -0,0 +1,30 @@
+﻿namespace AppForSEII2526.API.DTOs.ReturnProductsDTOs
+{
+
+}
+namespace AppForSEII2526.API.DTOs.ReturnProductsDTOs
+{
+    public class ReturnedProductInfoDTO
+    {
+        public ReturnedProductInfoDTO(
+            int quantity,
+            
+            string productName,
+            string brandName,
+            string warehouseLocation
+            
+        )
+        {
+            ProductName = productName;
+            BrandName = brandName;
+            WarehouseLocation = warehouseLocation;
+            Quantity = quantity;
+        }
+
+        public int PurchaseProductId { get; set; }
+        public string ProductName { get; set; }
+        public string BrandName { get; set; }
+        public string WarehouseLocation { get; set; }
+        public int Quantity { get; set; }
+    }
+}
