34e0065d2a4c40a8180823fe47d68d20ba765e6f
JAIME SÁNCHEZ PARADA <Jaime.Sanchez19@alu.uclm.es>
2025-12-03 10:36:37 +0100

    yes

---
+++ test/AppForSEII2526.UIT/UC_Ban/UC_BanUser_UIT.cs
@@ -0,0 +1,17 @@
+﻿using AppForMovies.UIT.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppForSEII2526.UIT.Shared;
+
+namespace AppForSEII2526.UIT.UC_Ban
+{
+    public class UC_BanUser_UIT : UC_UIT
+    {
+        public UC_BanUser_UIT(ITestOutputHelper output) : base(output)
+        {
+        }
+    }
+}
