ebb69e7abb29febaac607485c7309eeaab21f865
JAIME SÁNCHEZ PARADA <Jaime.Sanchez19@alu.uclm.es>
2025-12-03 10:41:28 +0100

    creating page object to interact with SelectUsersForBanning

---
+++ test/AppForSEII2526.UIT/UC_Ban/SelectUsersForBanning_PO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_Ban
+{
+    public class SelectUsersForBanning_PO : PageObject
+    {
+        public SelectUsersForBanning_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+    }
+}
