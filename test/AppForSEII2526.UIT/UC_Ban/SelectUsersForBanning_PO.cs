996f9ee0964dd7de185b417175fdaf9b70afb80d
JAIME SÁNCHEZ PARADA <Jaime.Sanchez19@alu.uclm.es>
2025-12-03 10:58:05 +0100

    Adding Locators. Interacting with theInterface to search

--- test/AppForSEII2526.UIT/UC_Ban/SelectUsersForBanning_PO.cs
+++ test/AppForSEII2526.UIT/UC_Ban/SelectUsersForBanning_PO.cs
@@ -8,8 +8,16 @@ namespace AppForSEII2526.UIT.UC_Ban
 {
     public class SelectUsersForBanning_PO : PageObject
     {
+        By banUser = By.Id("BanUser");
+        //other input actions
         public SelectUsersForBanning_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
         {
+        }
+        public void ClickBanUser(string username)
+        {
+           WaitForBeingClickable(banUser);
+            _driver.FindElement(banUser).SendKeys(username);
+           // completar : _driver.FindElement().Click();
         }
     }
 }
