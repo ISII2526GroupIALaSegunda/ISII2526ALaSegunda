7fbfd5e6dc9f74960fd7d5331ce9cbf959ab0a9e
alejandrogomezl <alejandrogomezlozano@hotmail.com>
2025-12-09 19:49:09 +0100

    we create the class for implementing the UIT test cases

---
+++ test/AppForSEII2526.UIT/UC_PurchaseDelivery/PurchaseDelivery.cs
@@ -0,0 +1,17 @@
+﻿using AppForMovies.UIT.Shared;
+using AppForSEII2526.UIT.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_PurchaseDelivery
+{
+    public class UC_PurchaseDeliveries_UIT : UC_UIT
+    {
+        public UC_PurchaseDeliveries_UIT(ITestOutputHelper output) : base(output)
+        {
+        }
+    }
+}
