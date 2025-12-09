f264e95d603d323ab36299490c1a1fb6ff330f22
alejandrogomezl <alejandrogomezlozano@hotmail.com>
2025-12-09 20:03:35 +0100

    [IMP] Creating the PageObject to interact with SelectPurchaseOrders.

---
+++ test/AppForSEII2526.UIT/UC_PurchaseDelivery/SelectPurchaseOrdersForDelivery_PO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_PurchaseDelivery
+{
+    public class SelectMoviesForRental_PO : PageObject
+    {
+        By inputPostalcode = By.Id("inputPostalcode");
+        By inputTotalPrice = By.Id("inputTotalPrice");
+        By buttonSearchPurchaseOrders = By.Id("searchPurchaseOrders");
+        public SelectMoviesForRental_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+        public void SearchPurchaseOrders(string postalcode)
+        {
+            //wait for the webelement to be clickable
+            WaitForBeingClickable(inputPostalcode);
+            _driver.FindElement(inputPostalcode).SendKeys(postalcode);
+            _driver.FindElement(buttonSearchPurchaseOrders).Click();
+
+
+        }
+    }
+}
