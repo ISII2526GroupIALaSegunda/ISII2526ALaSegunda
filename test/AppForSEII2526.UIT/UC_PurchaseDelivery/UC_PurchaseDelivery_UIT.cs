d6ede8829821ed73a3ab076105809ae48bc8ae7d
alejandrogomezl <alejandrogomezlozano@hotmail.com>
2025-12-10 22:41:55 +0100

    we develop the first filter: filter by postal code

--- test/AppForSEII2526.UIT/UC_PurchaseDelivery/UC_PurchaseDelivery_UIT.cs
+++ test/AppForSEII2526.UIT/UC_PurchaseDelivery/UC_PurchaseDelivery_UIT.cs
@@ -11,9 +11,17 @@ namespace AppForSEII2526.UIT.UC_PurchaseDelivery
     public class UC_PurchaseDeliveries_UIT : UC_UIT
     {
         private SelectPurchaseOrdersForDelivery_PO selectPurchaseOrdersForDelivery_PO;
+
+        private const int purchaseOrderId1 = 4;
+        private const string purchaseOrderCity1 = "Ab";
+        private const string purchaseOrderStreet1 = "Av españa";
+        private const string purchaseOrderPostalCode1 = "02002";
+        private const string purchaseOrderDate1 = "13/10/2025";
+        private const string purchaseOrderTotalPrice1 = "5";
+
         public UC_PurchaseDeliveries_UIT(ITestOutputHelper output) : base(output)
         {
-
+            selectPurchaseOrdersForDelivery_PO = new SelectPurchaseOrdersForDelivery_PO(_driver, _output);
         }
 
         private void Precondition_perform_login()
@@ -28,6 +36,24 @@ namespace AppForSEII2526.UIT.UC_PurchaseDelivery
             selectPurchaseOrdersForDelivery_PO.WaitForBeingVisible(By.Id("CreateDeliveryAssignment"));
             //we click on the menu
             _driver.FindElement(By.Id("CreateDeliveryAssignment")).Click();
+        }
+
+        [Fact(DisplayName = "UC1_AF1 – SelectPurchaseOrdersForDelivery Filtering")]
+        [Trait("UseCase", "UC1_AF1")]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void UC1_AF1_SelectPurchaseOrdersForDelivery_Filtering_Test()
+        {
+            //Arrange
+            InitialStepsForPurchaseDelivery();
+            var expectedPurchaseOrders = new List<string[]> { 
+                new string[] { purchaseOrderCity1, purchaseOrderStreet1, purchaseOrderPostalCode1, purchaseOrderDate1, purchaseOrderTotalPrice1 } 
+            };
+
+            //Act
+            selectPurchaseOrdersForDelivery_PO.SearchPurchaseOrders(purchaseOrderPostalCode1, "");
+
+            //Assert
+            Assert.True(selectPurchaseOrdersForDelivery_PO.CheckListOfPurchaseOrders(expectedPurchaseOrders));
         }
     }
 }
