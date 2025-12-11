74c892dc9a2668230092d753e1499ad928651305
alejandrogomezl <alejandrogomezlozano@hotmail.com>
2025-12-11 09:24:25 +0100

    UC2_BF_AF0 – GetDetailsDeliveryAssignment Success

---
+++ test/AppForSEII2526.UIT/UC_PurchaseDelivery/GetDetailsDeliveryAssignment_PO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_PurchaseDelivery
+{
+    public class GetDetailsDeliveryAssignment_PO : PageObject
+    {
+        public GetDetailsDeliveryAssignment_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+
+        public bool CheckDeliveryAssignmentDetail(string id, string deliveryDriverName, string personalMessage,
+            string deliveryAssignmentDone, string extraReward)
+        {
+            WaitForBeingVisible(By.Id("ExtraReward"));
+            bool result = true;
+            result = result && _driver.FindElement(By.Id("ID")).Text.Contains(id);
+            result = result && _driver.FindElement(By.Id("DeliveyDriverName")).Text.Contains(deliveryDriverName);
+            result = result && _driver.FindElement(By.Id("PersonalMessage")).Text.Contains(personalMessage);
+            result = result && _driver.FindElement(By.Id("DeliveryAssignmentDone")).Text.Contains(deliveryAssignmentDone);
+            result = result && _driver.FindElement(By.Id("ExtraReward")).Text.Contains(extraReward);
+
+            return result;
+
+        }
+
+        public bool CheckListOfPurchaseDeliveries(List<string[]> expectedPurchaseDeliveries)
+        {
+            return CheckBodyTable(expectedPurchaseDeliveries, By.Id("AssignedDeliveries"));
+        }
+    }
+}
