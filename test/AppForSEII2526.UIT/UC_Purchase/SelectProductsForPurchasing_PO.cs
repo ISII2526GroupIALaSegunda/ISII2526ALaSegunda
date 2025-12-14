16e3ebdbbbedc7c5810878f94a7adaffdb615aef
Ecomont <hector19.rl@gmail.com>
2025-12-14 18:06:39 +0100

    Add ids to purchase form fields and full purchase UI test

--- test/AppForSEII2526.UIT/UC_Purchase/SelectProductsForPurchasing_PO.cs
+++ test/AppForSEII2526.UIT/UC_Purchase/SelectProductsForPurchasing_PO.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using AppForSEII2526.UIT.Shared;
 
 namespace AppForSEII2526.UIT.UC_Purchase
@@ -16,6 +17,15 @@ namespace AppForSEII2526.UIT.UC_Purchase
         By productsTable = By.Id("productsTable");
         By btnPurchase = By.Id("btnPurchase");
         By errorBox = By.Id("errorBox");
+
+        By inputCustomerName = By.Id("NameCustomer");
+        By inputCustomerSurname = By.Id("SurnameCustomer");
+        By inputStreet = By.Id("Street");
+        By inputCity = By.Id("City");
+        By inputPostalCode = By.Id("PostalCode");
+        By selectPaymentMethod = By.Id("PaymentMethodId");
+        By btnConfirmPurchase = By.Id("btnConfirmPurchase");
+        By totalPrice = By.Id("TotalPrice");
 
         public SelectProductsForPurchasing_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
         {
@@ -63,6 +73,59 @@ namespace AppForSEII2526.UIT.UC_Purchase
             WaitForBeingVisible(btnPurchase);
             WaitForBeingClickable(btnPurchase);
             _driver.FindElement(btnPurchase).Click();
+        }
+
+        public void FillPurchaseForm(string name, string surname, string street, string city, string zip)
+        {
+            WaitForBeingVisible(inputCustomerName);
+            WaitForBeingVisible(inputCustomerSurname);
+            WaitForBeingVisible(inputStreet);
+            WaitForBeingVisible(inputCity);
+            WaitForBeingVisible(inputPostalCode);
+
+            _driver.FindElement(inputCustomerName).Clear();
+            _driver.FindElement(inputCustomerName).SendKeys(name);
+
+            _driver.FindElement(inputCustomerSurname).Clear();
+            _driver.FindElement(inputCustomerSurname).SendKeys(surname);
+
+            _driver.FindElement(inputStreet).Clear();
+            _driver.FindElement(inputStreet).SendKeys(street);
+
+            _driver.FindElement(inputCity).Clear();
+            _driver.FindElement(inputCity).SendKeys(city);
+
+            _driver.FindElement(inputPostalCode).Clear();
+            _driver.FindElement(inputPostalCode).SendKeys(zip);
+        }
+
+        public void SelectFirstAvailablePaymentMethod()
+        {
+            WaitForBeingVisible(selectPaymentMethod);
+            var select = new SelectElement(_driver.FindElement(selectPaymentMethod));
+
+            if (select.Options.Count <= 1)
+                throw new InvalidOperationException("No payment methods available for selection.");
+
+            select.SelectByIndex(1);
+        }
+
+        public void SubmitPurchaseForm()
+        {
+            WaitForBeingVisible(btnConfirmPurchase);
+            WaitForBeingClickable(btnConfirmPurchase);
+            _driver.FindElement(btnConfirmPurchase).Click();
+        }
+
+        public bool IsPurchaseCreated()
+        {
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(15));
+            bool navigated = wait.Until(d => d.Url.Contains("/purchase/detailpurchase") && d.Url.Contains("PurchaseID="));
+            if (!navigated)
+                return false;
+
+            WaitForBeingVisible(totalPrice);
+            return true;
         }
     }
 }
