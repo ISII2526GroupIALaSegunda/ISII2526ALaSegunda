2998e90c4583d525117463b0bb5280cfd03c6be4
JaimeSanchez19 <Jaime.Sanchez19@alu.uclm.es>
2026-05-12 09:26:12 +0200

    fixing errors

--- src/AppForSEII2526.Web/BanReportStateContainer.cs
+++ src/AppForSEII2526.Web/BanReportStateContainer.cs
@@ -1,53 +1,9 @@
 ﻿using AppForSEII2526.Web.API;
 
-
 namespace AppForSEII2526.Web
 {
     public class BanReportStateContainer
     {
-        public BanReportForCreateDTO BanReport { get; private set; } = new BanReportForCreateDTO()
-        {
-            Customers = new List<ReportCustomerForCreateDTO>()
-        };
-
-        public event Action? OnChange;
-
-        private void NotifyStateChanged() => OnChange?.Invoke();
-
-        public void AddCustomer(string customerId, string message)
-        {
-            if (!BanReport.Customers.Any(c => c.CustomerId == customerId))
-            {
-                BanReport.Customers.Add(new ReportCustomerForCreateDTO
-                {
-                    CustomerId = customerId,
-                    Message = message
-                });
-
-                NotifyStateChanged();
-            }
-        }
-
-        public void RemoveCustomer(ReportCustomerForCreateDTO customer)
-        {
-            BanReport.Customers.Remove(customer);
-            NotifyStateChanged();
-        }
-
-        public void ClearCustomers()
-        {
-            BanReport.Customers.Clear();
-            NotifyStateChanged();
-        }
-
-        public void BanReportProcessed()
-        {
-            BanReport = new BanReportForCreateDTO()
-            {
-                Customers = new List<ReportCustomerForCreateDTO>()
-            };
-
-            NotifyStateChanged();
-        }
+       
     }
 }
